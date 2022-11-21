8c29a0406cfec3af042a6470f9daf40fb8033edf
igbinosa idahosa <iidahosa@bell.net>
2022-11-21 06:31:06 -0500

    'Change Password, Recover Password' usecase is compleated. 'login To Account, Logout of Account' usecase is compleated

--- pages/urls.py
+++ pages/urls.py
@@ -9,7 +9,12 @@ urlpatterns = [
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
     path('signout', views.signout, name="signout"),
-    path('recoverpassword', views.recoverpassword, name="recoverpassword"),
-    path('activate/<uidb64>/<token>', views.activate, name="activate"),
 
+    path('recoverpassword', views.recoverpassword, name="recoverpassword"),
+    #uidb64 is the user id coded in base 64, token to check if password is valid
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
+    
+    path('reset/<uidb64>/<token>', views.reset, name="reset"),
+    path('resetpasswordconfirm', views.resetpasswordconfirm, name="resetpasswordconfirm"),
+ 
  ]
--- pages/views.py
+++ pages/views.py
@@ -151,10 +151,46 @@ def signout(request):
     return render(request, 'pages/signin.html')
 
 def recoverpassword(request):
+
+    if request.method == 'POST':
+        user_email = request.POST['email']
+        associated_user = User.objects.filter(email=user_email).first()
+        if associated_user:
+            current_site = get_current_site(request)
+            email_subject = "Password Reset request"
+            #the file which will hold the message being sent
+            message2 = render_to_string('templateactivateaccount.html',{
+            
+            'name': associated_user.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(associated_user.pk)),
+            'token': generate_token.make_token(associated_user)
+            })
+            email = EmailMessage(
+            email_subject,
+            message2,
+            settings.EMAIL_HOST_USER,
+            [associated_user.email],
+            )
+            email.fail_silently = True
+
+            if email.send():
+                messages.success(request, " Password reset sent. We emailed you instructions for setting your password. If an account exist you should  receive them shortly.")
+
+            else:
+                messages.error(request, "Problems sending reset password email")
+
+        return redirect('signin')
+        #return redirect(request, 'signin')
+            #email.fail_silently = True
+            #email.send()
+
+
     return render(request, 'pages/recoverpassword.html')
 
 def activate(request,uidb64,token):
     try:
+        #the id is uid
         uid = force_text(urlsafe_base64_decode(uidb64))
         myuser = User.objects.get(pk=uid)
     except (TypeError,ValueError,OverflowError,User.DoesNotExist):
@@ -169,3 +205,85 @@ def activate(request,uidb64,token):
         return redirect('index')
     else:
         return render(request,'activation_failed.html')
+
+
+def reset(request, uidb64,token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request,myuser)
+        messages.success(request, "You can change your password!!")
+        return redirect('resetpasswordconfirm')
+    else:
+        return render(request,'activation_failed.html')
+
+
+def resetpasswordconfirm(request):
+
+    if request.method == "POST":
+        password = request.POST.get("password")
+        cpassword = request.POST.get("cpassword")
+        
+        if password == cpassword:
+            # Saving to DB using Django ORM - the best way
+            current_user = request.user 
+            current_user.password
+            #h = (str)(current_user.id)
+            myuser = User.objects.get(pk = (str)(current_user.id))
+            #myuser = User.objects.get(pk=uid)
+
+            #myuser.password = password
+            myuser.set_password(password)
+            #myuser.username = myuser.username
+            myuser.save()
+            #User.objects.create(password=password)
+            #User.save()
+            messages.success(request, "Your password has been set. You may go ahead and <b>log in </b> now.")
+            return redirect('signin')
+        else:
+            messages.error(request, "The two passwords do not match")
+            return redirect('resetpasswordconfirm')
+
+    return render(request,'pages/resetpasswordconfirm.html')
+
+    #if request.method == 'POST':
+    #    return redirect('signin')
+    """
+    User = User
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        if request.method == 'POST':
+
+
+            form = SetPasswordForm(user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Your password has been set. You may go ahead and <b>log in </b> now.")
+                return redirect('signin')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+
+        form = SetPasswordForm(user)
+        return render(request, 'password_reset_confirm.html', {'form': form})
+    else:
+        messages.error(request, "Link is expired")
+
+    messages.error(request, 'Something went wrong, redirecting back to Homepage')
+    return redirect("signin")
+    """
+
+
+    return render(request,'pages/resetpasswordconfirm.html')
